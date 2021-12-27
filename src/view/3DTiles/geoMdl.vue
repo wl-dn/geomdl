f2e32f779dbb37998ed9efc7c315600927d494e4
wl-dn <2534819797@qq.com>
2021-12-27 11:24:06 +0800

    闻龙

--- src/view/3DTiles/geoMdl.vue
+++ src/view/3DTiles/geoMdl.vue
@@ -1774,11 +1774,11 @@ export default {
   mounted() {
     this.initCesium(); // cesim初始化必须放在mounted里面
     // 加载三维地形
-    // new TerrainLoader().loadLocalTerrain(
-    //   "http://192.10.3.237:81/tsyTerrain/",
-    //   viewer,
-    //   true
-    // );
+    new TerrainLoader().loadLocalTerrain(
+      "http://192.10.3.237:81/tsyTerrain/",
+      viewer,
+      true
+    );
     this.registerOnclickEvent();
     eventVue.$emit("sendCesiumViewer", viewer);
     // this.getGeoServerWmsBoundingBox("http://192.10.3.237/geoserver/crcc-dev/wms");
